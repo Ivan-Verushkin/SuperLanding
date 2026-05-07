15542a060034b4f1363af723096c7608efb904c6
ivan verushkin <ivanveruskin86@gmail.com>
2026-05-07 20:09:04 +0300

    Add new section Logo Presentation

--- script.js
+++ script.js
@@ -30,3 +30,61 @@ burger.addEventListener("click", function () {
         burgerIcon.src = "Images/burger.svg";
     }
 });
+
+const logoPreview = document.querySelector(".logo-preview");
+const logoMask = document.querySelector(".logo-mask");
+const logoText = document.querySelector(".logo-preview__text");
+
+const backgroundButtons = document.querySelectorAll("[data-bg]");
+const logoColorButtons = document.querySelectorAll("[data-logo]");
+
+let selectedBackgroundColor = "#0B0B0B";
+let selectedLogoColor = "#F7EEDA";
+
+function updateDisabledColors() {
+    backgroundButtons.forEach((button) => {
+        button.disabled = button.dataset.bg.toLowerCase() === selectedLogoColor.toLowerCase();
+    });
+
+    logoColorButtons.forEach((button) => {
+        button.disabled = button.dataset.logo.toLowerCase() === selectedBackgroundColor.toLowerCase();
+    });
+}
+
+backgroundButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        const newBackgroundColor = button.dataset.bg;
+
+        if (newBackgroundColor.toLowerCase() === selectedLogoColor.toLowerCase()) {
+            return;
+        }
+
+        selectedBackgroundColor = newBackgroundColor;
+        logoPreview.style.backgroundColor = selectedBackgroundColor;
+
+        updateDisabledColors();
+    });
+});
+
+logoColorButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        const newLogoColor = button.dataset.logo;
+
+        if (newLogoColor.toLowerCase() === selectedBackgroundColor.toLowerCase()) {
+            return;
+        }
+
+        selectedLogoColor = newLogoColor;
+
+        logoMask.style.backgroundColor = selectedLogoColor;
+        logoText.style.color = selectedLogoColor;
+
+        updateDisabledColors();
+    });
+});
+
+logoMask.style.backgroundColor = selectedLogoColor;
+logoText.style.color = selectedLogoColor;
+logoPreview.style.backgroundColor = selectedBackgroundColor;
+
+updateDisabledColors();
