d137648d572d922475331a1f4cddb79800aea643
ivan verushkin <ivanveruskin86@gmail.com>
2026-05-07 11:16:54 +0300

    Create repo

---
+++ script.js
@@ -0,0 +1,13 @@
+const header = document.querySelector(".header");
+const burger = header.querySelector(".burger-menu");
+const burgerIcon = header.querySelector(".burger-menu__icon");
+
+burger.addEventListener("click", function () {
+    header.classList.toggle("header--mobile");
+
+    if (header.classList.contains("header--mobile")) {
+        burgerIcon.src = "images/burger-exit.svg";
+    } else {
+        burgerIcon.src = "images/burger.svg";
+    }
+});
