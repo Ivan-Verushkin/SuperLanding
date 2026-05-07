f6ca76741c34df66a30ff036f2d513d3e5aa3b42
ivan verushkin <ivanveruskin86@gmail.com>
2026-05-07 11:32:45 +0300

    path fix, fix burger logic

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
 const header = document.querySelector(".header");
 const burger = header.querySelector(".burger-menu");
 const burgerIcon = header.querySelector(".burger-menu__icon");
+const nav = header.querySelector(".header__nav");
+
+function hideMobileNav() {
+    if (window.innerWidth <= 500) {
+        nav.style.display = "none";
+        header.classList.remove("header--mobile");
+        burgerIcon.src = "Images/burger.svg";
+    } else {
+        nav.style.display = "flex";
+    }
+}
+
+// При загрузке страницы
+hideMobileNav();
+
+// При изменении размера окна
+window.addEventListener("resize", hideMobileNav);
 
 burger.addEventListener("click", function () {
     header.classList.toggle("header--mobile");
 
     if (header.classList.contains("header--mobile")) {
-        burgerIcon.src = "images/burger-exit.svg";
+        nav.style.display = "flex";
+        burgerIcon.src = "Images/burger-exit.svg";
     } else {
-        burgerIcon.src = "images/burger.svg";
+        nav.style.display = "none";
+        burgerIcon.src = "Images/burger.svg";
     }
 });
