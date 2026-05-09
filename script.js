dc286dea1f9a7a7e7c9683a3a0f47ff3a523880f
ivan verushkin <ivanveruskin86@gmail.com>
2026-05-09 10:12:09 +0300

    add typewriter

--- script.js
+++ script.js
@@ -103,3 +103,40 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+const changingText = document.querySelector(".welcome__changing-text");
+
+const texts = [
+    "A Product Designer",
+    "A UI/UX Designer",
+    "A WEB Designer"
+];
+
+let index = 0;
+
+function restartTypingAnimation() {
+    changingText.style.animation = "none";
+    changingText.style.width = "auto";
+
+    const textWidth = changingText.scrollWidth;
+    const textLength = changingText.textContent.length;
+
+    changingText.style.setProperty("--steps", textLength);
+    changingText.style.setProperty("--text-width", `${textWidth}px`);
+
+    changingText.style.width = "0";
+
+    changingText.offsetHeight;
+
+    changingText.style.animation =
+        "typing 1.5s steps(var(--steps)) forwards, blink 1s step-end infinite";
+}
+
+function changeText() {
+    index = (index + 1) % texts.length;
+    changingText.textContent = texts[index];
+
+    restartTypingAnimation();
+}
+
+restartTypingAnimation();
+setInterval(changeText, 3000);
